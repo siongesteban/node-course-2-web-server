ace118db46685d5d9d4bd055273e69199b805ae0
Siong Esteban <siong.esteban@gmail.com>
2017-10-21 01:02:09 +0800

    Add projects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        welcomeMessage: 'Hello! This is my portfolio page!'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle this request'
